757e0fcf1746ec61223d3846195497ba539f2c4f
abimelec <carrillopoma@gmail.com>
2016-03-04 07:43:49 -0500

    iniciando proyecto

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="estilos.css" href="">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<div class="contenedor">
+<nav>
+   <ul class="menu-lista">
+		<li class="menu-item"><a href="" class="menu-link">INICIO</a></li>
+		<li class="menu-item"><a href="" class="menu-link">GALERIA</a></li>
+		<li class="menu-item"><a href="" class="menu-link">BLOG</a></li>
+		<li class="menu-item"><a href="" class="menu-link">ANUNCIOS</a></li>
+		<li class="menu-item"><a href="" class="menu-link">CONTÁCTENOS</a></li>
+	</ul>
+</nav>	
+<div class="slider">
+<img src="1.jpg" height="450" width="960">
+</div>
+
+<div class="slider">
+<img src="dd.jpg" height="180" width="180">
+</div>
+
+<div class="slider">
+<img src="3.jpg" height="180" width="180">
+</div>
+
+<div class="slider">
+<img src="4.jpg" height="180" width="180">
+</div>
+
+<div class="slider">
+<img src="5.jpg" height="180" width="180">
+
+</div>
+</body>
+</html>
+
